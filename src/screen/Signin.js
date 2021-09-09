3b02d04fe1d6865f8ca5ed35d0ef08b01ca8c483
Aprajati Siuli <aprajatisiuli002@gmail.com>
2021-09-10 05:23:23 +0530

    second commit

--- src/screen/Signin.js
+++ src/screen/Signin.js
@@ -173,7 +173,7 @@ const Signin = ({navigation}) => {
 
   const onGoogleButtonPress = async () => {
     // const {idToken} = await GoogleSignin.signIn();
-    // // Create a Google credential with the token
+    // Create a Google credential with the token
     // const googleCredential = auth.GoogleAuthProvider.credential(idToken);
     // // Sign-in the user with the credential
     // return await auth().signInWithCredential(googleCredential);
